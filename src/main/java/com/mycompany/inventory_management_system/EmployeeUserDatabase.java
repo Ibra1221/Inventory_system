cdfc20c5f65fd3a4aa1bb8273dbf8293a9df5e74
Pola Nasser <gjfcdg1@gmail.com>
2025-10-18 13:09:13 +0300

    Change createRecordFrom method to public

--- src/main/java/com/mycompany/inventory_management_system/EmployeeUserDatabase.java
+++ src/main/java/com/mycompany/inventory_management_system/EmployeeUserDatabase.java
@@ -15,7 +15,7 @@ public class EmployeeUserDatabase extends Database<EmployeeUser> {
     }
 
     @Override
-    protected EmployeeUser createRecordFrom(String line) {
+    public EmployeeUser createRecordFrom(String line) {
         String[] parts = line.split(",");
         if (parts.length == 5) {
             return new EmployeeUser(parts[0].trim(), parts[1].trim(), parts[2].trim(),parts[3].trim(),parts[4].trim());
