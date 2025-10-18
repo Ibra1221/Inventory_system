a323bcf2cff3924aa73af3cea6a1d941e4200e7a
PolaNasser13 <gjfcdg1@gmail.com>
2025-10-18 17:36:13 +0300

    MERGE CODES

--- src/main/java/com/mycompany/inventory_management_system/Inventory_management_system.java
+++ src/main/java/com/mycompany/inventory_management_system/Inventory_management_system.java
@@ -4,6 +4,8 @@
 
 package com.mycompany.inventory_management_system;
 
+import java.util.Scanner;
+
 /**
  *
  * @author pola-nasser13
@@ -11,6 +13,84 @@ package com.mycompany.inventory_management_system;
 public class Inventory_management_system {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
+        System.out.println("--Inventory Management System--");
+        System.out.println("1. Employee");
+        System.out.println("2. Admin");
+        System.out.println("3. Products");
+        System.out.println("4. Exit");
+        System.out.println("Choose 1 or 2 or 3 or 4");
+        
+        Scanner scan = new Scanner(System.in);
+        int choice = scan.nextInt();
+        scan.nextLine();
+        
+        switch(choice){
+            case 1:
+                System.out.println("\n\nEmployee:");
+                System.out.println("Adding new Employee:");
+                String id,name,email,address,phone;
+                int num = 0;
+                do{
+                    System.out.println("Enter employee id:");
+                    id = scan.nextLine();
+                    num = Integer.parseInt(id);
+                    if (num <= 0)
+                        System.out.println("Employee id must be positive! Please try again.");
+                } while(num <= 0);
+                
+                System.out.println("Enter employee name:");
+                name = scan.nextLine();
+                System.out.println("Enter employee email:");
+                email = scan.nextLine();
+                System.out.println("Enter employee address:");
+                address = scan.nextLine();
+                System.out.println("Enter employee phone number");
+                phone = scan.nextLine();
+                
+                EmployeeUser account = new EmployeeUser(id, name, email,address, phone);
+                System.out.println("\nEmployee added successfully!");
+                System.out.println("Employee Data: " + account.lineRepresentation());
+                break;
+                
+                
+            case 2:
+                //Admin stuff
+                //break;
+                
+            case 3:
+                System.out.println("\n\nProduct::");
+                System.out.println("Adding new Product:");
+                String pid,pname,mname,sname;
+                int pquantity;
+                float pprice;
+                int num2 = 0;
+                do{
+                    System.out.println("Enter product id:");
+                    pid = scan.nextLine();
+                    num = Integer.parseInt(pid);
+                    if (num <= 0)
+                        System.out.println("product id must be positive! Please try again.");
+                } while(num <= 0);
+                
+                System.out.println("Enter product name:");
+                pname = scan.nextLine();
+                System.out.println("Enter manufacturer name:");
+                mname = scan.nextLine();
+                System.out.println("Enter supplier name:");
+                sname = scan.nextLine();
+                System.out.println("Enter quantity:");
+                pquantity = scan.nextInt();
+                System.out.println("Enter price:");
+                pprice = scan.nextFloat();
+                
+                Product product = new Product(pid,pname,mname,sname,pquantity,pprice);
+                System.out.println("\nProduct added successfully!");
+                break;
+                
+                
+            case 4:
+                System.out.println("Exiting system...");
+                scan.close();
+                System.exit(0);
 }
+    }}
